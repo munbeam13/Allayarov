5f7c25158ed444e0d0dc751881296bf28d00c5da
munbeam13 <allayarov13a@gmail.com>
2024-09-10 10:50:17 +0300

    Запрос ввода символа

--- Lab0_1/Lab0_1.cpp
+++ Lab0_1/Lab0_1.cpp
@@ -3,5 +3,10 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello World!\n";
+	cout << "Hello World!"<<endl;
+	
+	char s;
+	cout << "Enter a symbol: ";
+	cin >> s;
+	cout << "Your symbol: " << s << endl;
 }
